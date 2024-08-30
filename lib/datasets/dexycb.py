c030749aca6113a6a8eeb3e0e5c054a338036f5a
MisakaVan <2102315149@qq.com>
2024-08-30 16:28:49 +0800

    feat.(dexycb.py) added DexYCB_Depth dataset that loads depth image.

--- lib/datasets/dexycb.py
+++ lib/datasets/dexycb.py
@@ -250,6 +250,53 @@ class DexYCB(HDataset):
         shape = np.array(shape, dtype=np.float32)
         return shape
 
+@DATASET.register_module()
+class DexYCB_Depth(DexYCB):
+
+    def __init__(self, cfg):
+        """
+        cfg.N_CHANNELS: 1 or 3
+            specify the number of channels of the output depth image.
+            the depth image in the disk is 1-channel, but in more cases,
+            a 3-channel image better fits the existing rgb processing code.
+        """
+        super().__init__(cfg)
+        self.n_channels = cfg.get("N_CHANNELS", 3)
+        assert self.n_channels in [1, 3], f"Only supports 1 or 3 channels"
+
+    def get_image(self, idx,):
+        """
+        Returns the depth image. Shape: (H, W, C)
+        If n_channels is 1, return the depth image. Shape: (H, W, 1)
+        If n_channels is 3, the depth image is repeated to 3 channels. Shape: (H, W, 3)
+        """
+        path = self.get_image_path(idx)
+
+        img = (
+            np.array(
+                imageio.imread(path),
+                dtype=np.float32,
+            )
+            / 65535.0
+        )
+        # The image in the disk is 16-bit quantized.
+        # But due to the fact that torch.from_numpy does not support uint16,
+        # and that cv2.warpAffine does not support int32,
+        # here image is loaded as float32 and then quantized to 0-1.
+        
+        img = img[:, :, np.newaxis]  # add a channel dimension
+        if self.n_channels == 3:
+            img = np.repeat(img, 3, axis=2)
+        return img
+
+    def get_image_path(self, idx):
+        """
+        Returns the path of the depth image.
+        """
+        ori_idx = self.sample_idxs[idx]
+        sample = self.dex_ycb[ori_idx]
+        return sample["depth_file"]
+
 
 @DATASET.register_module()
 class DexYCBMultiView(torch.utils.data.Dataset):
